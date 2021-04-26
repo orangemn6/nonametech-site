dd9c7a96f48dc265f8cec7d51f6530437fe9f437
orangemn6 <jbensa081@gmail.com>
2021-04-26 18:05:33 -0400

    please be fixed

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -5,7 +5,7 @@ export default function Contact() {
     <div className="contact">
       <h2 className="contact__title">Any questions? <span className="colored">Contact us.</span></h2>
       <p className="contact__paragraph">We're looking forward to hearing from you. Feel free to contact us if you have any questions!</p>
-      <a href="https://snipcart.com/contact-feedback">
+      <a href="mailto:NonameCustomsandTech@gmail.com">
         <button>Contact Us</button>
       </a>
     </div>
